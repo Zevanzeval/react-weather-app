8705665a3c3846bf9908e87e764b2c8758280924
Zevanzeval <sazevalmiel@gmail.com>
2024-05-08 11:45:48 +0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App{" "}
-      <footer>
-        This project was coded by Oleksandra Holovei and is{" "}
-        <a
-          href="https://github.com/Zevanzeval/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App{" "}
+        <footer>
+          This project was coded by Oleksandra Holovei and is{" "}
+          <a
+            href="https://github.com/Zevanzeval/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
