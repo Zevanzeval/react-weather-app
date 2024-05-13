24c89860c5715f0bd2af4d37e44b56f9fda33155
Zevanzeval <sazevalmiel@gmail.com>
2024-05-13 17:42:22 +0800

    Added fahrenheit unit

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,11 +15,14 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           {" "}
-          <img src={props.data.iconUrl} alt={props.data.description} />{" "}
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>
-          <span className="unit">°C</span>
+          <div className="d-flex">
+            <div>
+              <img src={props.data.iconUrl} alt={props.data.description} />
+            </div>{" "}
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
