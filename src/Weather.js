e536b85cdfc1a510fb5b695ec62bccc2ed318a46
Zevanzeval <sazevalmiel@gmail.com>
2024-05-16 13:13:24 +0800

    Added a new component WeatherForecast to the project

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import WeatherInfo from "./WeatherInfo";
-//import WeatherForecast from "./WeatherForecast";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -62,6 +62,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
