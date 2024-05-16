c719f0d8ca474e1a60b8ef73546df0b2b407bee2
Zevanzeval <sazevalmiel@gmail.com>
2024-05-16 12:50:05 +0800

    Changed icon to animated one

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,9 +17,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           {" "}
           <div className="d-flex">
-            <div>
-              <img src={props.data.iconUrl} alt={props.data.description} />
-            </div>{" "}
+            <WeatherIcon code={props.data.icon} />{" "}
             <div>
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
